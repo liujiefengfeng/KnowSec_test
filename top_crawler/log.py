5bc7ac5422225d2b659ada8e2a3a3ecac008a665
Alkaid-seven <18345151786@163.com>
2014-11-09 23:08:21 +0800

    modify2

--- top_crawler/log.py
+++ top_crawler/log.py
@@ -1,50 +1,37 @@
-
-# -*- encoding= utf-8 -*-
 import logging
 import logging.handlers
 import os
-import sys
+import time
+from  datetime import datetime
 
-LOG_LEVELS = {
-    
-    'NOTSET': logging.NOTSET,
-    'DEBUG': logging.DEBUG,
-    'INFO': logging.INFO,
-    'WARNING': logging.WARNING,
-    'ERROR': logging.ERROR,
-    'CRITICAL': logging.CRITICAL
-}
+def log_service( logfilename,
+            loglevel = logging.INFO,
+            formatstring = "%(asctime)s -<%(lineno)d> %(levelname)s: \t%(message)s ",
+            debug = False,
+            splitby = 'time',
+    ):
 
-
-def config_logging( file_name,log_level,logs_dir ):
-    '''
-    @summary: config logging to write logs to local file
-    @param file_name: name of log file
-    @param log_level: log level
-    '''
-
-    if not logs_dir:
-        logs_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logs")
-    if os.path.exists(logs_dir) and os.path.isdir(logs_dir):
-        pass
+    _dir = "./"
+    if not os.path.exists(_dir):
+        os.mkdir(_dir)
+    _format = logging.Formatter (formatstring)
+    log = logging.getLogger(_dir + logfilename + "__")
+    if splitby == 'time':
+        fh = logging.handlers.TimedRotatingFileHandler(_dir + logfilename, 'W0')
+    elif splitby == 'size':
+        fh = logging.handlers.RotatingFileHandler(_dir + logfilename, maxBytes=1024 * 1024 * 10, backupCount=10)
     else:
-        os.makedirs(logs_dir)
+        fh = logging.FileHandler(_dir + logfilename)
+    fh.setFormatter(_format)
+    fh.setLevel(loglevel)
+    log.addHandler(fh)
+    log.setLevel(loglevel)
 
-    file_name = os.path.join(logs_dir, file_name)
-
-    #set a logfile rotating function
-    rotatingFileHandler = logging.FileHandler( filename =file_name)
-    formatter = logging.Formatter("%(asctime)s %(name)-12s %(lineno)3d %(levelname)-8s %(message)s")
-    rotatingFileHandler.setFormatter(formatter)
-    logging.getLogger().addHandler(rotatingFileHandler)
-
-    console = logging.StreamHandler()  #控制台输出
-    formatter = logging.Formatter("%(name)-12s: %(lineno)d %(levelname)-8s %(message)s") 
-    console.setFormatter(formatter)
-    logging.getLogger().addHandler(console)
-
-    logger = logging.getLogger("")
-    level = LOG_LEVELS [ log_level.upper() ]
-    logger.setLevel(level)
-
-
+    if debug:
+        debughandler = logging.StreamHandler()
+        debughandler.setLevel(logging.DEBUG)
+        debughandler.setFormatter(_format)
+        log.addHandler(debughandler)
+        log.setLevel(logging.DEBUG)
+    return log
+   
